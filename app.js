4c68df6833d3871ee5559ed834880c2dd5e14454
bbubbush <bbubbush@gmail.com>
2023-01-08 13:03:40 +0900

    Feat: Login 화면 만들기

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('Hello JS');
+document.addEventListener("DOMContentLoaded", function(){
+  const HIDDEN_CLASS_NAME = 'hidden'
+  const USERNAME_KEY = 'username'
+
+  const showGreeting = (username) => {
+    const greeting = document.querySelector('#greeting');
+    greeting.innerHTML = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASS_NAME)
+  }
+
+  const savedUsername = localStorage.getItem(USERNAME_KEY)
+  if (!savedUsername) {
+    const loginForm = document.querySelector('#login-form');
+    loginForm.classList.remove(HIDDEN_CLASS_NAME)
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const username = document.querySelector('#login-form input').value;
+      localStorage.setItem(USERNAME_KEY, username)
+      loginForm.classList.add(HIDDEN_CLASS_NAME);
+      
+      showGreeting(username);
+    })
+    return;
+  }
+  showGreeting(savedUsername);
+})
+
+
+
+
+
+
